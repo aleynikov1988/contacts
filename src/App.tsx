3e8aa917d1d099e408166537db73793de848c2fe
Maxim Aleynikov <max.aleyinikov@gmail.com>
2022-05-26 12:21:51 +0300

    clear stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Link, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './pages/Home';
+import { List } from './pages/List';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <h1>Contacts</h1>
+      <nav className="menu">
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/list">List</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet /> */}
     </div>
   );
 }
-
-export default App;
